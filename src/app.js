68a9887400ec495835a72aaec8d3d8b09cbe3ccd
singh-anita <anita.singh@raweng.com>
2022-03-17 16:25:00 +0530

    data file added

---
+++ src/app.js
@@ -0,0 +1,1 @@
+import { getData } from  './data/data.js'
--- src/data/data.js
+++ src/data/data.js
@@ -1,1 +1,25 @@
-const data = 'https://sayalijoshi27.github.io/JS-Tasks/webpage-data.json'
+const dataURL = 'https://sayalijoshi27.github.io/JS-Tasks/webpage-data.json'
+
+// export const getData = (url) =>{
+//   fetch(url)
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.log(error));
+// } 
+
+// getData(dataURL);
+
+
+export const getData = async () =>{
+ let data = await fetch(dataURL)
+  data = await data.json();
+  // console.log('data', data)
+  return data;
+}
+// console.log('--getdata',getData())
+// getData();
+
+
+// getData().then(data => {
+//   console.log('data', data)
+// })
