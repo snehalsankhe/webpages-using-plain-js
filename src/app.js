2dc5b758cf64a3f1d59ece1c2c1bed3fe1673c6b
dipti-patil <dipti23.patil@gmail.com>
2022-03-22 16:22:41 +0530

    created banner component & rendered the same

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import { getData } from "./data/data.js";
 import { Navigation } from "./components/navigation.js";
+import { Banner } from "./components/banner.js";
 
 const root = document.getElementById('root')
 const renderpage = async () => {
     let data = await getData();
     root.appendChild(Navigation(data.navData));
+    root.appendChild(Banner(data.bannerData))
 }
 renderpage();
 
