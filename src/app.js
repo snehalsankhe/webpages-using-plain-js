e1f249291f01318a17c4d77f6ee4cac49b77a6af
singh-anita <anita.singh@raweng.com>
2022-03-22 16:08:10 +0530

    added logo in navigation component

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
 import { getData } from "./data/data.js";
 import { Navigation } from "./components/navigation.js";
-// import { Logo } from '../src/components/logo.js'
 
 const root = document.getElementById('root')
 const renderpage = async () => {
     let data = await getData();
     console.log('---ff', data)
     root.appendChild(Navigation(data.navData));
-    // root.appendChild (Logo(data.navData))
 }
 renderpage();
 
