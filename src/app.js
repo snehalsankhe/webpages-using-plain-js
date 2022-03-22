1ed37e352c88ef8b34b7f2bb261a6a520f555819
singh-anita <anita.singh@raweng.com>
2022-03-22 15:36:36 +0530

    logo added

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import { fetchData } from  './data/data.js'
+import { Logo } from '../src/components/logo.js'
 const root = document.getElementById("root");
 const dataURL = 'https://sayalijoshi27.github.io/JS-Tasks/webpage-data.json'
 
 const renderData = async () => {
   let response = await fetchData(dataURL)
   console.log('----response', response)
-  return response
-
-  // root.appendChild()
+  const { data : navData } = response;
+  // return response
+  console.log('dat----', navData)
+  root.appendChild (Logo(navData.logo))
 }
 
 renderData();
--- src/components/logo.js
+++ src/components/logo.js
@@ -1,8 +1,13 @@
-export const Logo = () =>{
+import { navData } from "../data/data.js";
+
+export const Logo = (data) =>{
   const navbarBrand = document.createElement('div');
   navbarBrand.classList.add('company-logo');
   let logoImg =  document.createElement('img');
-  logoImg.classList.add('logo');
+  logoImg.src = data.imgSrc;
+  logoImg.alt = data.alt;
+  logoImg.classList.add('logoImg');
+
   navbarBrand.appendChild(logoImg);
   return navbarBrand;
 }
