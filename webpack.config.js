44180e5122fdfe01d50272821ac92a401a73c959
snehalsankhe <snehal.sankhe@raweng.com>
2022-04-05 12:58:09 +0530

    modify index file path

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const { CleanWebpackPlugin } =require('clean-webpack-plugin');
-const MiniCssExtractPlugin  =  require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
@@ -9,23 +8,9 @@ const config = {
     filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
   },
-  module: {
-    rules: [
-      { 
-        test: /\.css$/, 
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader' 
-        ]
-      },
-    ],
-  },
   mode: 'none',
   plugins: [
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename:'../css/style.[contenthash].css'
-    }),
     new HtmlWebpackPlugin({ template: './index.html' })
   ],
 };
